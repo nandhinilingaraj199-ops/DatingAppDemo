cf8e811b68fad847aaf899dd4ad9379fc1a41d45
Nandhini L <nandhinilingaraj199@gmail.com>
2026-02-17 21:56:13 +0530

    Angular set up

--- API/Program.cs
+++ API/Program.cs
@@ -10,9 +10,23 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
 builder.Services.AddDbContext<AppDbContext>(Options=>Options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddCors();
+
+// Configure HTTPS with mkcert certificates
+// builder.WebHost.ConfigureKestrel(serverOptions =>
+// {
+//     serverOptions.ListenLocalhost(5001, listenOptions =>
+//     {
+//         listenOptions.UseHttps("certs/cert.pem", "certs/key.pem");
+//     });
+// });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseCors(x=> x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200","https://localhost:4200"));
 
 app.MapControllers();
 
